db3be7635ac9eb61b98de4a6da8e50a66476c0f1
Afif-Mac <afifmansib123@gmail.com>
2023-12-02 12:18:26 +0700

    Day1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Editor } from '@monaco-editor/react';
+import { useState } from 'react';
+
+const files = {
+  "script.py" : {
+    name : "script.py",
+    language : "python",
+    value : "this is a python text",
+  },
+  "index.html": {
+    name : "index.html",
+    language : "html",
+    value : "<div> </div>"
+  }
+}
 
 function App() {
+  const [filesname, setfilename] = useState("script.py")
+  const file = files[filesname]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={()=>{setfilename("index.html")}}>Swich to HTML</button>
+        <button onClick={()=>{setfilename("script.py")}}>Swich to HTML</button>
+        <Editor
+        height="100vh"
+        width="100%"
+        theme='vs-dark'
+        path={file.name}
+        defaultLanguage={file.language}
+        defaultValue={file.value}
+        ></Editor>
       </header>
     </div>
   );
